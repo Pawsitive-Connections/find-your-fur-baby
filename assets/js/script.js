cf8c0f277c9855c0209b9683d5a0b4c2d2931c3e
chasewaldron2400 <chasew2400@gmail.com>
2024-07-08 21:32:58 -0500

    jsRevision

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-const apiKey = 'nRPoBjRs5ZStqGdhxXi3zA==vDeVKQVySaNORCHC'
-const url = `https://api.api-ninjas.com/v1/dogs?name=beagle&X-Api-Key=${apiKey}`
-
-fetch(url)
-.then(response => response.json())
-.then(data => {
-console.log(data)
-})
-.catch(error => {
-console.error('error fetching data:', error)
-})
 
 $(document).ready(() => {
     const questions = $('.question');
