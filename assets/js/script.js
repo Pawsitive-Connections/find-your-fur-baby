15d9774800cc53f686ec70518f49e5a8c7d6f3d5
Patrick Hennessey <pjhennessey89@gmail.com>
2024-07-09 21:20:52 -0500

    Added my questions to index, added submit functionality, working on quiz logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
 $(document).ready(() => {
     const questions = $('.question');
+    let answers = {};
 
     const showQuestion = (currentQuestion) => {
         questions.each(function (index) {
@@ -26,5 +27,64 @@ $(document).ready(() => {
     });
 
     showQuestion(0);
+
+    $('.quizBtn').click(function() {
+        const question = $(this).data('question');
+        const answer = $(this).data('answer');
+        answers[question] = answer;
+        console.log(answers);
+    });
+
+    $('#submit').click(function() {
+        getBreeds(answers)
+    })
+
+    
+    
 });
 
+function getBreeds(answers) {
+    const apiKey = `0RPs3+YvKx3lcRmxrMI8tQ==K6WaN0Jvv5ptwUKa`;
+    const url = `https://api.api-ninjas.com/v1/dogs`;
+    console.log(apiKey);
+    console.log(url);
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'X-Api-Key': apiKey
+        }
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Response not found.')
+        } 
+        return response.json();
+    })
+    .then(data => {
+        const breeds = findBreeds(data, answers);
+        console.log(breeds);
+    })
+    .catch(error => {
+        console.error(error);
+    }); 
+};
+
+
+function findBreeds(breeds, answers) {
+    return breeds.filter(breed => {
+        return (
+            breed.good_with_children === answers.q1 &&
+            breed.good_with_other_dogs === answers.q2 &&
+            breed.shedding === answers.q3 &&
+            breed.grooming === answers.q4 &&
+            breed.drooling === answers.q5 &&
+            breed.coat_length === answers.q6 &&
+            breed.good_with_strangers === answers.q7 &&
+            breed.playfulness === answers.q8 &&
+            breed.protectiveness === answers.q9 &&
+            breed.trainability === answers.q10 &&
+            breed.energy === answers.q11 &&
+            breed.barking === answers.q12
+        );
+    });
+}
